bfd7cb3469f9a871253cef4fc037fc60ac9a7e6d
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-03-27 04:08:37 +0300

    finished it

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const menuButton = document.querySelector('.menu');
-function toggle() {
-}
-menuButton.addEventListener('mouseover', () => {
-  document.querySelector('.items').style.display = 'block';
+const items = document.querySelector('.items');
+menuButton.addEventListener('click', () => {
+//   if (items.style.display === 'none') {
+//     items.style.display = 'block';
+//   } else {
+//     items.style.display = 'none';
+//   }
+  menuButton.classList.toggle('active');
 });
 
-menuButton.addEventListener('mouseleave', () => {
-  document.querySelector('.items').style.display = 'none';
-});
+// menuButton.addEventListener('mouseleave', () => {
+//   document.querySelector('.items').style.display = 'none';
+// });
